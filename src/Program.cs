a0dea10f4b80a8928fc1f6834fb7e16699a6c66a
polatengin <enpolat@microsoft.com>
2024-09-01 13:00:54 -0700

    removing redundant code

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -32,21 +32,4 @@ host.Run();
 
 public class ProjectDbContext : DbContext
 {
-  private readonly ILogger<ProjectDbContext> _logger;
-
-  public ProjectDbContext(DbContextOptions<ProjectDbContext> options, ILogger<ProjectDbContext> logger)
-      : base(options)
-  {
-    _logger = logger;
-  }
-
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    if (!optionsBuilder.IsConfigured)
-    {
-      optionsBuilder.UseSqlServer("YourConnectionString")
-          .EnableSensitiveDataLogging()
-          .LogTo(_logger.LogInformation, LogLevel.Information);
-    }
-  }
 }
