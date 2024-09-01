566827d29fb8c7b7656678ef2e6a341a125c5470
polatengin <enpolat@microsoft.com>
2024-09-01 13:01:04 -0700

    formatting document

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,28 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureServices((context, services) =>
-            {
-              services.AddApplicationInsightsTelemetryWorkerService(options =>
-              {
-                options.ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTIONSTRING");
-              });
+IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+  .ConfigureServices((context, services) =>
+  {
+    services.AddApplicationInsightsTelemetryWorkerService(options =>
+    {
+      options.ConnectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTIONSTRING");
+    });
 
-              services.AddLogging(builder =>
-              {
-                builder.ClearProviders();
-                builder.AddApplicationInsights();
-              });
+    services.AddLogging(builder =>
+    {
+      builder.ClearProviders();
+      builder.AddApplicationInsights();
+    });
 
-              services.AddDbContext<ProjectDbContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING")));
-            });
+    services.AddDbContext<ProjectDbContext>(options =>
+      options
+        .UseSqlServer(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING"))
+        .EnableSensitiveDataLogging()
+    );
+  });
 
 using var host = CreateHostBuilder(args).Build();
 
