98882f8bd67797af67e02af5da1416ea1b20cb01
polatengin <enpolat@microsoft.com>
2024-09-01 12:54:37 -0700

    adding dbcontext implementation

--- src/Program.cs
+++ src/Program.cs
@@ -2,3 +2,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+public class ProjectDbContext : DbContext
+{
+  private readonly ILogger<ProjectDbContext> _logger;
+
+  public ProjectDbContext(DbContextOptions<ProjectDbContext> options, ILogger<ProjectDbContext> logger)
+      : base(options)
+  {
+    _logger = logger;
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
+    {
+      optionsBuilder.UseSqlServer("YourConnectionString")
+          .EnableSensitiveDataLogging()
+          .LogTo(_logger.LogInformation, LogLevel.Information);
+    }
+  }
+}
